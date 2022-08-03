5ad31cdfd49bd2582a1342e6eb1369d375aab8e7
Walace Silva <wsilva.emp@gmail.com>
2022-08-03 23:12:47 +0000

    feat: limita e ordena as transações no painel de controle

--- app/controllers/backoffice/overview_controller.rb
+++ app/controllers/backoffice/overview_controller.rb
@@ -2,6 +2,6 @@ class Backoffice::OverviewController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @transactions = Transaction.all
+    @transactions = Transaction.order(created_at: :desc).limit(5)
   end
 end
