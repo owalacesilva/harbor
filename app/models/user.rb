7673cb9febdca7fa10a6dd9978e3b1b97922f6fe
Walace Silva <wsilva.emp@gmail.com>
2022-08-16 00:17:08 +0000

    feat: adicionado validação de email no cadastro de usuários

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   include Profilable
 
+  attr_accessor :email_confirmation, :security_password_confirmation
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -13,8 +15,10 @@ class User < ApplicationRecord
   has_many :banking_accounts, dependent: :destroy
   has_many :withdraws, dependent: :destroy
   has_many :purchases, dependent: :destroy
+  has_many :transactions, dependent: :destroy
 
   validates :email, presence: true
+  validates :email, confirmation: true
 
   before_create :create_user_wallet
 
