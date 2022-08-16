71c5693704f1ee4e818a27062d918fd505c06183
Walace Silva <wsilva.emp@gmail.com>
2022-08-15 22:21:06 -0300

    feat: Criado menu de navegação

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+
+  def active_class(path)
+    'active' if current_page?(path)
+  end
+
+  def active_class_multiple(paths)
+    paths.include?(request.path) ? 'active' : ''
+  end
 end
