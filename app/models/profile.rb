c0c85faa68672e96fc163dad85e62c8dea9f55c5
Walace Silva <wsilva.emp@gmail.com>
2022-08-15 22:03:08 -0300

    refactor: validação para enumeration genders

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -11,5 +11,5 @@ class Profile < ApplicationRecord
 
   validates :first_name, presence: true
   validates :gender, presence: true
-  validates :gender, inclusion: %w[male famale undefined]
+  validates :gender, inclusion: genders.keys
 end
