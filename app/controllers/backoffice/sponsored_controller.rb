60b2b9611a65b16645fa95b23a86ec7a9f1a7fa9
Walace Silva <wsilva.emp@gmail.com>
2022-08-26 13:31:13 -0300

    feat: criar filtros na listagem de indicados

--- app/controllers/backoffice/sponsored_controller.rb
+++ app/controllers/backoffice/sponsored_controller.rb
@@ -4,6 +4,7 @@ class Backoffice::SponsoredController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @filters = define_filters('sponsored')
     @sponsored = User.order(id: :desc).page(params[:page])
       .per(params[:limit])
   end
